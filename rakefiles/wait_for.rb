fd647634fa2bc4e42d825804bbd798b8f814b967
Tyler Roscoe <tyler@cryptio.net>
2017-09-02 12:15:32 -0600

    Add max_wait_secs.
    Mitigation for my recent troubles getting the dev environment to converge in CI.

--- rakefiles/wait_for.rb
+++ rakefiles/wait_for.rb
@@ -1,5 +1,6 @@
-def wait_for(cmd, sleep_secs=20)
+def wait_for(cmd, sleep_secs=20, max_wait_secs=15*60)
   ready = false
+  total_time = 0
   until ready
     sh cmd do |ok, res|
       if ok
@@ -7,6 +8,11 @@ def wait_for(cmd, sleep_secs=20)
         ready = true
       else
         puts "...not ready yet. Sleeping #{sleep_secs}s..."
+        total_time += sleep_secs
+        if total_time >= max_wait_secs
+          puts "Max wait time of #{max_wait_secs}s reached. Giving up."
+          return false
+        end
         sleep sleep_secs
       end
     end
