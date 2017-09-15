416a8d644e87537a856022320a33dd990d544fa8
Tyler Roscoe <tyler@cryptio.net>
2017-09-15 12:16:11 -0600

    Display smarter timeout message.

--- rakefiles/wait_for.rb
+++ rakefiles/wait_for.rb
@@ -7,7 +7,7 @@ def wait_for(args, run_with=method(:sh), sleep_secs=20, max_wait_secs=15*60)
         puts "...ready!"
         ready = true
       else
-        puts "...not ready yet. Sleeping #{sleep_secs}s (will wait for #{max_wait_secs}s total)..."
+        puts "...not ready yet. Sleeping #{sleep_secs}s (will give up in #{max_wait_secs - total_time}s)..."
         total_time += sleep_secs
         if total_time >= max_wait_secs
           raise "Max wait time of #{max_wait_secs}s reached. Giving up."
