c5c4f72278aeaa309a58c38ee2e0f827bf3b9a63
Tyler Roscoe <tyler@cryptio.net>
2017-10-08 13:55:20 -0600

    Add couchdb PV and PVCs that are AZ-specific.
    Preparation for couchdb service with more than one instance.

--- modules/volume/main.tf
+++ modules/volume/main.tf
@@ -1,9 +1,20 @@
-resource "aws_ebs_volume" "couchdb" {
+resource "aws_ebs_volume" "couchdb_us-east-2a" {
   availability_zone = "us-east-2a"
   size = 5
   encrypted = true
   tags {
-    Name = "couchdb pv ${var.environment}"
+    Name = "${var.environment} couchdb us-east-2a pv"
+    Environment = "${var.environment}"
+    Terraform = true
+  }
+}
+
+resource "aws_ebs_volume" "couchdb_us-east-2b" {
+  availability_zone = "us-east-2b"
+  size = 5
+  encrypted = true
+  tags {
+    Name = "${var.environment} couchdb us-east-2b pv"
     Environment = "${var.environment}"
     Terraform = true
   }
--- modules/volume/vars.tf
+++ modules/volume/vars.tf
@@ -1,5 +1,9 @@
 variable "environment" {}
 
-output "couchdb_volume_id" {
-  value = "${aws_ebs_volume.couchdb.id}"
+output "couchdb_us-east-2a_volume_id" {
+  value = "${aws_ebs_volume.couchdb_us-east-2a.id}"
+}
+
+output "couchdb_us-east-2b_volume_id" {
+  value = "${aws_ebs_volume.couchdb_us-east-2b.id}"
 }
