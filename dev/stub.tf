277ff7846453240d51f09d3ab0afc3cf02058111
Tyler Roscoe <tyler@cryptio.net>
2017-10-12 21:19:54 -0600

    Quell error when running terraform output.

--- dev/stub.tf
+++ dev/stub.tf
@@ -1,1 +1,5 @@
 # Mostly-empty file to prevent 'terragrunt apply-all' from freaking out.
+
+output "stub_output" {
+  value = "This unused output quells an error message when running 'terraform output' against this empty module."
+}
