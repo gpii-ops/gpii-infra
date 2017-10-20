72f866c1ca5d6ec9d492f7becabee1ab564ab802
Alfredo Matas <amatas@gmail.com>
2017-10-20 14:45:27 +0200

    Fix Persisten Volume assignment

--- modules/volume/vars.tf
+++ modules/volume/vars.tf
@@ -7,3 +7,7 @@ output "couchdb_us-east-2a_volume_id" {
 output "couchdb_us-east-2b_volume_id" {
   value = "${aws_ebs_volume.couchdb_us-east-2b.id}"
 }
+
+output "couchdb_us-east-2c_volume_id" {
+  value = "${aws_ebs_volume.couchdb_us-east-2c.id}"
+}
