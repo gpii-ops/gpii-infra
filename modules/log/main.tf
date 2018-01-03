bb01058647bca6b62d10b0707a89a9a19c69880b
Alfredo Matas <amatas@gmail.com>
2018-01-03 16:17:39 +0100

    Fix variable assignment

--- modules/log/main.tf
+++ modules/log/main.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudwatch_log_group" "main" {
-  name = "${var.cluster_name}.gpii.net"
+  name = "${var.cluster_name}"
 
 # The logs shouldn't be removed between deployments, but we have to set the
 # scope in terraform to avoid issues at the 'detroy' step.
--- modules/log/vars.tf
+++ modules/log/vars.tf
@@ -1,4 +1,4 @@
-variable "environment" {}
+variable "cluster_name" {}
 
 output "cluster_log_name" {
   value = "${aws_cloudwatch_log_group.main.name}"
