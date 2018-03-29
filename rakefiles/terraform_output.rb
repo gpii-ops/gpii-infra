40eef290d93ff47010efc0dad0d13e00b74cd0a1
Tyler Roscoe <tyler@cryptio.net>
2018-03-29 17:20:50 -0600

    wait_for doesn't handle exceptions, so callback with (ok, res) instead.

--- rakefiles/terraform_output.rb
+++ rakefiles/terraform_output.rb
@@ -17,15 +17,22 @@ def terraform_output_runner(dir, tmpdir, &block)
   terraform_cmd_output = `#{terraform_cmd}`
   terraform_cmd_ok = $?
   if terraform_cmd_ok != 0
-    raise "terraform_output failed with return code #{terraform_cmd_ok}. Terraform output was:\n#{terraform_cmd_output}\n"
+    ok = false
+    msg = "terraform_output failed with return code #{terraform_cmd_ok}. Terraform output was:\n#{terraform_cmd_output}\n"
+    block.call(ok, msg)
+    return
   end
 
   jq_cmd = "echo '#{terraform_cmd_output}' | jq -s add"
   jq_cmd_output = `#{jq_cmd}`
   jq_ok = $?
   if jq_ok == 0
-    block.call(jq_ok, JSON.parse(jq_cmd_output))
+    ok = true
+    msg = JSON.parse(jq_cmd_output)
+    block.call(ok, msg)
   else
-    raise "terraform_output failed with return code #{jq_ok}. Terraform output was:\n#{terraform_cmd_output}\njq output was:\n#{jq_cmd_output}\n"
+    ok = false
+    msg = "terraform_output failed with return code #{jq_ok}. Terraform output was:\n#{terraform_cmd_output}\njq output was:\n#{jq_cmd_output}\n"
+    block.call(ok, msg)
   end
 end
