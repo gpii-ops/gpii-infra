baab9f3e964ac8237b3e3b2589955b7a11f40919
Tyler Roscoe <tyler@cryptio.net>
2018-03-29 13:14:50 -0600

    Wrap terraform_output with wait_for for robustness.
    This is in response to the intermittent errors described in 0e1ccb49.

--- rakefiles/terraform_output.rb
+++ rakefiles/terraform_output.rb
@@ -1,6 +1,18 @@
 require "json"
+require_relative "./wait_for.rb"
 
 def terraform_output(dir, tmpdir)
+  args = [dir, tmpdir]
+  return wait_for(
+    args,
+    run_with: method(:terraform_output_runner),
+    sleep_secs: 3,
+    max_wait_secs: 9,
+    verbose: true,
+  )
+end
+
+def terraform_output_runner(dir, tmpdir, &block)
   terraform_cmd = "cd #{dir} && TMPDIR=#{tmpdir} terragrunt output-all --terragrunt-non-interactive -json --terragrunt-ignore-dependency-errors"
   terraform_cmd_output = `#{terraform_cmd}`
   terraform_cmd_ok = $?
@@ -12,7 +24,7 @@ def terraform_output(dir, tmpdir)
   jq_cmd_output = `#{jq_cmd}`
   jq_ok = $?
   if jq_ok == 0
-    return JSON.parse(jq_cmd_output)
+    block.call(jq_ok, JSON.parse(jq_cmd_output))
   else
     raise "terraform_output failed with return code #{jq_ok}. Terraform output was:\n#{terraform_cmd_output}\njq output was:\n#{jq_cmd_output}\n"
   end
