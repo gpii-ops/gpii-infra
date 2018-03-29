0e1ccb491cb816e3e387dc30120edafdafd3b82b
Tyler Roscoe <tyler@cryptio.net>
2018-03-29 12:11:49 -0600

    Add additional logging/error-handling to terraform_output.
    This is in response to intermittent errors that look like this:
    
    parse error: Expected separator between values at line 25, column 34
    
    I'd like to see the json that jq is trying to parse.

--- rakefiles/terraform_output.rb
+++ rakefiles/terraform_output.rb
@@ -1,12 +1,19 @@
 require "json"
 
 def terraform_output(dir, tmpdir)
-  cmd = "cd #{dir} && TMPDIR=#{tmpdir} terragrunt output-all --terragrunt-non-interactive -json --terragrunt-ignore-dependency-errors | jq -s add"
-  res = `#{cmd}`
-  ok = $?
-  if ok == 0
-    return JSON.parse(res)
+  terraform_cmd = "cd #{dir} && TMPDIR=#{tmpdir} terragrunt output-all --terragrunt-non-interactive -json --terragrunt-ignore-dependency-errors"
+  terraform_cmd_output = `#{terraform_cmd}`
+  terraform_cmd_ok = $?
+  if terraform_cmd_ok != 0
+    raise "terraform_output failed with return code #{terraform_cmd_ok}. Terraform output was:\n#{terraform_cmd_output}\n"
+  end
+
+  jq_cmd = "echo '#{terraform_cmd_output}' | jq -s add"
+  jq_cmd_output = `#{jq_cmd}`
+  jq_ok = $?
+  if jq_ok == 0
+    return JSON.parse(jq_cmd_output)
   else
-    raise "terraform_output failed with return code #{ok}. Output was:\n#{res}"
+    raise "terraform_output failed with return code #{jq_ok}. Terraform output was:\n#{terraform_cmd_output}\njq output was:\n#{jq_cmd_output}\n"
   end
 end
