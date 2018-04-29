337cded7dbd5efca2c0972340f32d25ff5a866bd
Tyler Roscoe <tyler@cryptio.net>
2018-04-28 19:19:43 -0600

    These are not needed (or even valid).

--- gcp/modules/administration-tasks/main.tf
+++ gcp/modules/administration-tasks/main.tf
@@ -6,8 +6,6 @@ variable "secrets_dir" {}
 
 module "administration_tasks" {
   source = "/exekube-modules/helm-template-release"
-  ###tiller_namespace = "kube-system"
-  ###client_auth      = "${var.secrets_dir}/kube-system/helm-tls"
 
   release_name      = "administration-tasks"
   release_namespace = "gpii"
