b3b6e28abb3ca266b7e9ef81882b82843972d614
Tyler Roscoe <tyler@cryptio.net>
2018-05-22 19:18:46 -0600

    Establish gcp/rakefiles/vars.rb.
    This includes rspec-based unit tests.

---
+++ gcp/rakefiles/tests/spec/vars_spec.rb
@@ -0,0 +1,30 @@
+require "../vars.rb"
+
+describe Vars do
+  it "set_vars requires ENV['TF_VAR_project_id']" do
+    allow(ENV).to receive(:[]=)
+    allow(ENV).to receive(:[]).with("TF_VAR_project_id").and_return(nil)
+    expect { Vars.set_vars() }.to raise_error(ArgumentError, "TF_VAR_project_id must be set")
+  end
+
+  it "set_vars sets default vars" do
+    allow(ENV).to receive(:[]=)
+    allow(ENV).to receive(:[])
+    allow(ENV).to receive(:[]).with("TF_VAR_project_id").and_return("fake-project-id")
+    Vars.set_vars()
+    expect(ENV).to have_received(:[]=).with("ENV", "dev")
+    expect(ENV).to have_received(:[]=).with("ORGANIZATION_ID", "247149361674")
+    expect(ENV).to have_received(:[]=).with("BILLING_ID", "01A0E1-B0B31F-349F4F")
+  end
+
+  it "set_vars doesn't clobber vars that are already set" do
+    allow(ENV).to receive(:[]=)
+    allow(ENV).to receive(:[])
+    allow(ENV).to receive(:[]).with("ENV").and_return("fake-env")
+    allow(ENV).to receive(:[]).with("TF_VAR_project_id").and_return("fake-project-id")
+    Vars.set_vars()
+    expect(ENV).not_to have_received(:[]=).with("ENV", "dev")
+  end
+end
+
+# vim: set et ts=2 sw=2:
---
+++ gcp/rakefiles/vars.rb
@@ -0,0 +1,17 @@
+class Vars
+  def self.set_vars
+    if ENV["TF_VAR_project_id"].nil?
+      puts "ERROR: TF_VAR_project_id must be set!"
+      puts "Do this: export TF_VAR_project_id=<your name>-<env>"
+      puts "and try again."
+      raise ArgumentError, "TF_VAR_project_id must be set"
+    end
+    if ENV["ENV"].nil?
+      ENV["ENV"] = "dev"
+    end
+    ENV["ORGANIZATION_ID"] = "247149361674"  # RtF Organization
+    ENV["BILLING_ID"] = "01A0E1-B0B31F-349F4F"  # RtF Billing Account
+  end
+end
+
+# vim: set et ts=2 sw=2:
