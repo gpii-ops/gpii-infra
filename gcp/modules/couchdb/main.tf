3929e30cd338cbf4f80343482abc1b293f2bb722
Sergey Nazarov <natarajaya@gmail.com>
2018-06-06 12:47:19 -0700

    GPII-2960: Changes for CouchDB Helm chart

--- gcp/modules/couchdb/main.tf
+++ gcp/modules/couchdb/main.tf
@@ -2,13 +2,15 @@ terraform {
   backend "gcs" {}
 }
 
-variable "secrets_dir" {}
+variable "values_dir" {}
 
 module "couchdb" {
+
   source = "/exekube-modules/helm-template-release"
 
   release_name      = "couchdb"
   release_namespace = "gpii"
+  release_values    = "${var.values_dir}/couchdb.yaml"
 
-  chart_name    = "couchdb/"
+  chart_name    = "chart/"
 }
