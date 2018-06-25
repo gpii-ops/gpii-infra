196f551207fb20c5fe4cc4b5740621d9a269af07
Sergey Nazarov <natarajaya@gmail.com>
2018-06-24 22:17:39 -0700

    Small fixes

--- gcp/modules/couchdb/main.tf
+++ gcp/modules/couchdb/main.tf
@@ -26,23 +26,23 @@ resource "null_resource" "couchdb_finish_cluster" {
   provisioner "local-exec" {
     command = <<EOF
       while [ "$PODS_READY" != "true" ]; do
-        PODS_READY=true
-        echo Waiting for all CouchDB pods to become Running...
-        for status in $(kubectl get pods --namespace ${var.release_namespace} -l app=couchdb -o jsonpath='{.items[*].status.phase}'); do
-          if [ "$status" != "Running" ]; then
-            PODS_READY=false
+        PODS_READY="true"
+        echo "Waiting for all CouchDB pods to become Running..."
+        for STATUS in $(kubectl get pods --namespace ${var.release_namespace} -l app=couchdb -o jsonpath='{.items[*].status.phase}'); do
+          if [ "$STATUS" != "Running" ]; then
+            PODS_READY="false"
           fi
         done
-        sleep 5;
+        sleep 10
       done
-      while [ "$STATUS" != "\"Cluster is already finished\"" ]; do
+      while [ "$STATUS" != '"Cluster is already finished"' ]; do
         RESULT=$(
           kubectl exec --namespace ${var.release_namespace} couchdb-couchdb-0 -c couchdb -- \
           curl -s http://${var.couchdb_admin_username}:${var.couchdb_admin_password}@127.0.0.1:5984/_cluster_setup \
-          -X POST -H 'Content-Type: application/json' -d '{"action": "finish_cluster"}');
-        echo CouchDB returned: $RESULT;
-        STATUS=$(echo $RESULT | jq ".reason");
-        sleep 5;
+          -X POST -H 'Content-Type: application/json' -d '{"action": "finish_cluster"}')
+        echo "CouchDB returned: $RESULT"
+        STATUS=$(echo $RESULT | jq ".reason")
+        sleep 10
       done
     EOF
   }
