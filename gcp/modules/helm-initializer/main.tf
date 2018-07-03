7f8c9297fd7f9b31fd946f48ba8e7a4a5d7508c4
Sergey Nazarov <natarajaya@gmail.com>
2018-07-02 22:45:25 -0700

    Workaround for 'context deadline exceeded' Helm issue

--- gcp/modules/helm-initializer/main.tf
+++ gcp/modules/helm-initializer/main.tf
@@ -15,3 +15,11 @@ module "system_tiller" {
   secrets_dir      = "${var.secrets_dir}"
   tiller_namespace = "${var.tiller_namespace}"
 }
+
+resource "null_resource" "wait_for_tiller" {
+  depends_on = ["module.system_tiller"]
+
+  provisioner "local-exec" {
+    command = "sleep 60"
+  }
+}
