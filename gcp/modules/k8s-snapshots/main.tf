4f5e2313cbcd9b8d16ecafd48438651537248fd5
Sergey Nazarov <natarajaya@gmail.com>
2018-07-10 20:48:08 -0700

    We don't need to patch volumes anymore

--- gcp/modules/k8s-snapshots/main.tf
+++ gcp/modules/k8s-snapshots/main.tf
@@ -2,19 +2,6 @@ terraform {
   backend "gcs" {}
 }
 
-variable "prd_deltas" {
-  default = "PT5M PT60M PT24H P7D P52W"
-}
-
-variable "stg_deltas" {
-  default = "PT15M PT60M PT4H PT24H P7D"
-}
-
-variable "dev_deltas" {
-  default = "PT5M PT15M PT45M"
-}
-
-variable "env" {}
 variable "secrets_dir" {}
 
 module "k8s-snapshots" {
@@ -26,20 +13,4 @@ module "k8s-snapshots" {
   release_namespace = "kube-system"
 
   chart_name = "../../../../../charts/k8s-snapshots"
-}
-
-resource "null_resource" "enable_backups" {
-  depends_on = ["module.k8s-snapshots"]
-
-  provisioner "local-exec" {
-    command = <<EOF
-      for PV in $(kubectl get pv -o jsonpath="{range .items[*]}{.metadata.name}{' '}"); do
-        if [ -z "$(kubectl get pv $PV -o jsonpath="{.metadata.annotations.backup\.kubernetes\.io/deltas}")" ]; then
-          kubectl patch pv $PV -p '{"metadata": {"annotations": {"backup.kubernetes.io/deltas": "${
-            var.env == "prd" ? "${var.prd_deltas}" : (var.env == "stg" ? "${var.stg_deltas}" : "${var.dev_deltas}")
-          }"}}}'
-        fi
-      done
-    EOF
-  }
 }
