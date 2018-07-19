72fe31278e7e05c81f8446e60f7e7c1490fe4042
Alfredo Matas <amatas@gmail.com>
2018-07-19 18:36:42 +0200

    Fix some resources and create DNS zone

--- common/modules/gcp-project/main.tf
+++ common/modules/gcp-project/main.tf
@@ -9,6 +9,12 @@ variable "project_owner" {}
 variable "billing_account" {}
 variable "organization_id" {}
 
+# compute the project_id to get the dns zone in JSON format
+# I wasn't able to replicate this function using HCL code
+data "external" "calculate_dns_zone" {
+  program = ["python", "-c", "print(\"{\\\"zone\\\": \\\"\" + \".\".join(\"${google_project.project.project_id}\".split(\"-\")[::-1]) + \".net.\") + \"\\\"}\""]
+}
+ 
 resource "google_project" "project" {
   name            = "gpii-gcp-${var.project_name}"
   project_id      = "gpii-gcp-${var.project_name}"
@@ -27,8 +33,8 @@ resource "google_project_services" "project" {
 }
 
 resource "google_project_iam_binding" "project" {
-  project     = "${google_project.project.project_id}"
-  role        = "roles/owner"
+  project = "${google_project.project.project_id}"
+  role    = "roles/owner"
   members = [
     "user:${var.project_owner}",
   ]
@@ -40,15 +46,23 @@ resource "google_service_account" "project" {
   project      = "${google_project.project.project_id}"
 }
 
-resource "google_service_account_iam_binding" "admin-account-iam" {
-  service_account_id = "${google_service_account.project.unique_id}"
+resource "google_project_iam_binding" "admin-account-iam" {
+  project = "${google_project.project.project_id}"
   role    = "roles/owner"
   members = [
     "serviceAccount:${google_service_account.project.email}",
   ]
 }
 
+resource "google_dns_managed_zone" "prod" {
+  project     = "${google_project.project.project_id}"
+  name        = "${google_project.project.project_id}-zone"
+  dns_name    = "${lookup(data.external.calculate_dns_zone.result, "zone")}"
+  description = "${google_project.project.project_id} DNS zone"
+}
+
 resource "google_storage_bucket" "project-tfstate" {
+  project     = "${google_project.project.project_id}"
   name = "gpii-gcp-${var.project_name}-tfstate"
   versioning = {
     enabled = "true"
