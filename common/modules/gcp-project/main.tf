a1e9ba46ea164696ea051887b7ae9a0e58ed2447
Alfredo Matas <amatas@gmail.com>
2018-08-17 10:48:57 +0200

    Fix escape characters in the regex

--- common/modules/gcp-project/main.tf
+++ common/modules/gcp-project/main.tf
@@ -29,7 +29,7 @@ locals  {
   dnsname = "${replace(
               replace(
                 google_project.project.name,
-                "/([\w]+)-([\w]+)-([\w]+)-?([\w]+)?/",
+                "/([\\w]+)-([\\w]+)-([\\w]+)-?([\\w]+)?/",
                 "$4.$3.$2.$1.net."),
               "/^\\./",
               "")
