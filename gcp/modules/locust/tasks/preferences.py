eafdd07e873d9da1ea1305a3efa85c8fd086f206
Sergey Nazarov <natarajaya@gmail.com>
2018-08-20 14:58:28 -0700

    Adding locust/tasks into .gitignore

--- gcp/modules/locust/tasks/preferences.py
+++ gcp/modules/locust/tasks/preferences.py
@@ -9,6 +9,18 @@ class GpiiTasks(TaskSet):
   def vladimir(self):
       self.client.get("/preferences/vladimir")
 
+  @task
+  def wayne(self):
+      self.client.get("/preferences/wayne")
+
+  @task
+  def omar(self):
+      self.client.get("/preferences/omar")
+
+  @task
+  def telugu(self):
+      self.client.get("/preferences/telugu")
+
 class GpiiWarmer(HttpLocust):
   task_set = GpiiTasks
   min_wait = 1000
