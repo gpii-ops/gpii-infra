d9b726573ba12304c6aa2921a289ea4745af1b5a
Alfredo Matas <amatas@gmail.com>
2018-08-30 13:30:19 +0200

    Add organization name parameter for the dns name of the helm charts

--- gcp/modules/gpii-preferences/main.tf
+++ gcp/modules/gpii-preferences/main.tf
@@ -5,6 +5,7 @@ terraform {
 variable "env" {}
 variable "secrets_dir" {}
 variable "charts_dir" {}
+variable "organization_domain" {}
 
 variable "dns_zones" {
   type = "map"
@@ -21,7 +22,7 @@ data "template_file" "preferences_values" {
 
   vars {
     env                    = "${var.env}"
-    dns_name               = "${var.dns_zones["${var.env}-gcp-gpii-net"]}"
+    dns_name               = "${var.dns_zones["${var.env}-gcp-${replace(var.organization_domain, ".", "-")}"]}"
     preferences_repository = "${var.preferences_repository}"
     preferences_checksum   = "${var.preferences_checksum}"
     couchdb_admin_username = "${var.secret_couchdb_admin_username}"
