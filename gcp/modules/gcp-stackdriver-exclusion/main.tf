3046e40cc09bc65e84f05a373bd4da7706799ee0
Tyler Roscoe <tyler@cryptio.net>
2018-09-25 16:18:52 -0600

    Move exclusions from env level to module level.
    Thanks Stepan. :)

--- gcp/modules/gcp-stackdriver-exclusion/main.tf
+++ gcp/modules/gcp-stackdriver-exclusion/main.tf
@@ -5,7 +5,20 @@ terraform {
 variable "project_id" {}
 variable "serviceaccount_key" {}
 variable "exclusions" {
-  default = {}
+  default = {
+    # Calico logs don't always set 'severity' correctly, so instead we match on
+    # 'textPayload'.
+    "calico-info" = "resource.type=container AND logName = (projects/__var.project_id__/logs/calico-node OR projects/__var.project_id__/logs/calico-typha) AND textPayload:\"[INFO]\""
+
+    "kubelet-discovered-runtime-cgroups" = "resource.type=gce_instance AND logName=projects/__var.project_id__/logs/kubelet AND jsonPayload.MESSAGE:\"Discovered runtime cgroups name: /system.slice/docker.service\""
+
+    "kubelet-healthz-200" = "resource.type=gce_instance AND logName=projects/__var.project_id__/logs/kubelet AND jsonPayload.MESSAGE:GET /healthz AND jsonPayload.MESSAGE:\" 200 \""
+
+    # The trailing space is to differentiate status code 200 ("GET /_up 200 ok
+    # 0") from a string that happens to start with 200 ("Setting calico-typha
+    # requests["cpu"] = 200m").
+    "kubelet-stats-summary-200" = "resource.type=gce_instance AND logName=projects/__var.project_id__/logs/kubelet AND jsonPayload.MESSAGE:GET /stats/summary AND jsonPayload.MESSAGE:\" 200 \""
+  }
 }
 
 module "gcp_stackdriver_exclusion" {
