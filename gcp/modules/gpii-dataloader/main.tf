60f3d427c8ecb445fb96de1c2f6fe365bda2dcfd
Stepan Stipl <stepan@stipl.net>
2018-10-12 15:21:10 +0100

    GPII-3391 - Hack to stop dataloader related errors

--- gcp/modules/gpii-dataloader/main.tf
+++ gcp/modules/gpii-dataloader/main.tf
@@ -15,8 +15,13 @@ data "template_file" "dataloader_values" {
   template = "${file("values.yaml")}"
 
   vars {
-    dataloader_repository  = "${var.dataloader_repository}"
-    dataloader_checksum    = "${var.dataloader_checksum}"
+    dataloader_repository = "${var.dataloader_repository}"
+
+    # This is an ugly hack to stop TF/Helm dataloader errors and should be removed
+    # in scope of https://github.com/gpii-ops/gpii-infra/pull/163
+    # dataloader_checksum    = "${var.dataloader_checksum}"
+    dataloader_checksum = "sha256:3876e3526b8b59f94aa25c8b6d1a3166df115d402b51176ef8bd91c899430369"
+
     couchdb_admin_username = "${var.secret_couchdb_admin_username}"
     couchdb_admin_password = "${var.secret_couchdb_admin_password}"
   }
