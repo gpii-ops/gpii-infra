195f2133cb3ca176cacd7a71c74b77a864433cc6
Alfredo Matas <amatas@gmail.com>
2018-11-08 19:32:31 +0100

    Add Project IAM Admin to the project SA

--- common/modules/gcp-project/main.tf
+++ common/modules/gcp-project/main.tf
@@ -123,6 +123,14 @@ data "google_iam_policy" "admin" {
       "serviceAccount:projectowner@${var.project_id}.iam.gserviceaccount.com",
     ]
   }
+
+  binding {
+    role = "roles/resourcemanager.projectIamAdmin"
+
+    members = [
+      "serviceAccount:${google_service_account.project.email}",
+    ]
+  }
 }
 
 provider "google" {
