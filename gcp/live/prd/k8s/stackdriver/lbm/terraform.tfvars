a4e98cd5603eb6a42373f1c13c8743af2c35f772
Sergey Nazarov <natarajaya@gmail.com>
2018-11-26 15:45:08 -0800

    Add gcp-stackdriver-lbm module

---
+++ gcp/live/prd/k8s/stackdriver/lbm/terraform.tfvars
@@ -0,0 +1,12 @@
+# ↓ Module metadata
+terragrunt = {
+  terraform {
+    source = "/project/modules//gcp-stackdriver-lbm"
+  }
+
+  include = {
+    path = "${find_in_parent_folders()}"
+  }
+}
+
+# ↓ Module configuration (empty means all default)
---
+++ gcp/modules/gcp-stackdriver-lbm/main.tf
@@ -0,0 +1,50 @@
+terraform {
+  backend "gcs" {}
+}
+
+variable "nonce" {}
+variable "project_id" {}
+variable "serviceaccount_key" {}
+
+# Enables debug mode when TF_VAR_stackdriver_debug is not empty
+variable "stackdriver_debug" {
+  default = ""
+}
+
+resource "null_resource" "apply_stackdriver_lbm" {
+  triggers = {
+    nonce = "${var.nonce}"
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+      export PROJECT_ID=${var.project_id}
+      export GOOGLE_CLOUD_KEYFILE=${var.serviceaccount_key}
+      export STACKDRIVER_DEBUG=${var.stackdriver_debug}
+
+      RETRIES=5
+      RETRY_COUNT=1
+      while [ "$STACKDRIVER_DID_NOT_FAIL" != "true" ]; do
+        STACKDRIVER_DID_NOT_FAIL="true"
+        echo "[Try $RETRY_COUNT of $RETRIES] Applying Stackdriver resources..."
+        ruby -e '
+          require "/rakefiles/stackdriver.rb"
+          resources = read_resources("${path.module}/resources")
+          apply_resources(resources)
+        '
+        if [ "$?" != "0" ]; then
+          STACKDRIVER_DID_NOT_FAIL="false"
+        fi
+
+        RETRY_COUNT=$(($RETRY_COUNT+1))
+        if [ "$RETRY_COUNT" == "$RETRIES" ]; then
+          echo "Retry limit reached, giving up!"
+          exit 1
+        fi
+        if [ "$STACKDRIVER_DID_NOT_FAIL" == "false" ]; then
+          sleep 10
+        fi
+      done
+    EOF
+  }
+}
