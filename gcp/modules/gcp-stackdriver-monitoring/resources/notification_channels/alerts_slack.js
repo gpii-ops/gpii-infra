be128cf09744f85168ec9db553935fb8ce029723
Sergey Nazarov <natarajaya@gmail.com>
2018-12-03 21:16:28 -0800

    Add Slack notification channel

---
+++ gcp/modules/gcp-stackdriver-monitoring/resources/notification_channels/alerts_slack.js
@@ -0,0 +1,13 @@
+{
+  "type": "slack",
+  "labels": {
+    "channel_name": "#alerts"
+  },
+  "user_labels": {},
+  "enabled": {
+    "value": true
+  },
+  "immutable": {
+    "value": true
+  }
+}
