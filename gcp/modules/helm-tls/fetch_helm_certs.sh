889d03b948a20da8ed8f7905662d584343a9a5a6
Sergey Nazarov <natarajaya@gmail.com>
2019-02-14 16:08:06 -0800

    Use double quotes for consistency

--- gcp/modules/helm-tls/fetch_helm_certs.sh
+++ gcp/modules/helm-tls/fetch_helm_certs.sh
@@ -3,7 +3,7 @@
 cd /project/live/${ENV}/k8s/kube-system/helm-tls
 
 echo "[helm-tls] Pulling helm-tls TF state..."
-state=$(terragrunt state pull 2> /dev/null | jq -r '.modules[].resources | select(length > 0)')
+state=$(terragrunt state pull 2> /dev/null | jq -r ".modules[].resources | select(length > 0)")
 for i in ca_cert helm_cert helm_key tiller_cert tiller_key; do
   content=$(echo "$state" | jq -r ".[\"local_file.${i}\"].primary.attributes.content")
   filename=$(echo "$state" | jq -r ".[\"local_file.${i}\"].primary.attributes.filename")
