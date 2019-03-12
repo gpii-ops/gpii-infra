24ba2b8366d054f82c2b0344c39b2ba0bb07fa2b
Alfredo Matas <amatas@gmail.com>
2019-03-12 17:38:12 +0100

    remove S3 TF variables

--- gcp/modules/backup-exporter/main.tf
+++ gcp/modules/backup-exporter/main.tf
@@ -4,13 +4,9 @@ terraform {
 
 variable "secrets_dir" {}
 variable "charts_dir" {}
+variable "destination_bucket" {}
 
 # Terragrunt variables
-
-# Secret variables
-
-variable "aws_access_key_id" {}
-variable "aws_secret_access_key" {}
 
 variable "service_account_name" {
   default = "backup-exporter@${var.project_id}.iam.gserviceaccount.com"
@@ -24,7 +20,7 @@ data "template_file" "backup-exporter" {
     aws_secret_access_key = "${var.aws_secret_access_key}"
     service_account_json  = "${google_service_account_key.service_account_json.private_key}"
     service_account_name  = "${var.service_account_name}"
-    s3_bucket             = "s3://${var.project_id}"
+    destination_bucket    = "${var.destination_bucket}"
   }
 }
 
