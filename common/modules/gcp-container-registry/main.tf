ae474e1c0ad6a5f7fd67283b91765b38f1e8990f
Tyler Roscoe <tyler@cryptio.net>
2019-03-28 18:07:43 -0600

    Change to IAM with 'allUsers' (thanks Stepan). Drop bucket since we can't manage it.

--- common/modules/gcp-container-registry/main.tf
+++ common/modules/gcp-container-registry/main.tf
@@ -19,37 +19,34 @@ locals {
   infra_multi_region = "${substr(var.infra_region, 0, 2)}"
 }
 
-resource "google_storage_bucket" "registry" {
-  # This name is prescribed by Google
-  name = "artifacts.${var.project_id}.appspot.com"
-
-  location = "${local.infra_multi_region}"
-
-  force_destroy = false
-
-  versioning = {
-    enabled = "false"
-  }
-}
-
-resource "google_storage_bucket_acl" "public_readable" {
-  bucket = "${google_storage_bucket.registry.name}"
-
-  role_entity = [
-    "READER:AllUsers",
-  ]
-}
-
 resource "google_service_account" "gcr_uploader" {
   account_id   = "gcr-uploader"
   display_name = "gcr-uploader"
 }
 
+# GCR stores data in a Google Storage bucket. The name of this bucket is
+# prescribed by Google. However, if we try to create the bucket ourselves (e.g.
+# with a 'google_storage_bucket' resource), we get 'Error 403: The bucket you
+# tried to create requires domain ownership verification.'
+#
+# Instead, we must let Google create the bucket for us by pushing to the
+# Registry. We handle this as a manual one-time step -- see XXX.
+#
+# We let Google create the bucket and manage its settings. We use the IAM
+# bindings below to control read and write access to the bucket (and hence to
+# the Registry).
 resource "google_storage_bucket_iam_binding" "gcr_uploader" {
-  bucket = "${google_storage_bucket.registry.name}"
+  bucket = "artifacts.${var.project_id}.appspot.com"
   role   = "roles/storage.admin"
 
   members = [
     "serviceAccount:${google_service_account.gcr_uploader.email}",
   ]
+}
+
+resource "google_storage_bucket_iam_member" "public_readable" {
+  bucket = "artifacts.${var.project_id}.appspot.com"
+  role   = "roles/storage.objectViewer"
+
+  member = "allUsers"
 }
