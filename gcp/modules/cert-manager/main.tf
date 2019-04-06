01690517bc31df49ec8269b828f0f77f307d30fa
Tyler Roscoe <tyler@cryptio.net>
2019-04-05 22:18:09 -0600

    Migrate cert-manager to GCR.

--- gcp/modules/cert-manager/main.tf
+++ gcp/modules/cert-manager/main.tf
@@ -5,14 +5,27 @@ terraform {
 variable "nonce" {}
 variable "secrets_dir" {}
 variable "charts_dir" {}
+variable "cert_manager_repository" {}
+variable "cert_manager_tag" {}
+
+data "template_file" "cert_manager_values" {
+  template = "${file("values.yaml")}"
+
+  vars {
+    cert_manager_repository = "${var.cert_manager_repository}"
+    cert_manager_tag        = "${var.cert_manager_tag}"
+  }
+}
 
 module "cert-manager" {
   source           = "/exekube-modules/helm-release"
   tiller_namespace = "kube-system"
   client_auth      = "${var.secrets_dir}/kube-system/helm-tls"
 
-  release_name      = "cert-manager"
-  release_namespace = "kube-system"
+  release_name            = "cert-manager"
+  release_namespace       = "kube-system"
+  release_values          = ""
+  release_values_rendered = "${data.template_file.cert_manager_values.rendered}"
 
   chart_name = "${var.charts_dir}/cert-manager"
 }
