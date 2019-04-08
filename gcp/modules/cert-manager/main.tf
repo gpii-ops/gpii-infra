5fd7e91e6a81624d0f4ad3cc0eb25fcc9b8846a7
Stepan Stipl <stepan@stipl.net>
2019-04-08 19:35:54 +0100

    Revert "Merge pull request #346 from stepanstipl/fix-dns-challenge"
    This reverts commit 371ed7d73e0e907e71ade05c4b37022f54f5d319, reversing
    changes made to 73ced69577cad16cd4fbbfe81527ed47f37f62cc.

--- gcp/modules/cert-manager/main.tf
+++ gcp/modules/cert-manager/main.tf
@@ -6,7 +6,6 @@ variable "nonce" {}
 variable "secrets_dir" {}
 variable "charts_dir" {}
 variable "project_id" {}
-variable "serviceaccount_key" {}
 
 provider "google" {
   project     = "${var.project_id}"
