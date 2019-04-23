b9b22ba4ab08539eacc9185c6a5efb7c639e6577
Tyler Roscoe <tyler@cryptio.net>
2019-04-23 00:22:17 -0600

    Switch from image.checksum to image.tag.

--- gcp/modules/couchdb-prometheus-exporter/main.tf
+++ gcp/modules/couchdb-prometheus-exporter/main.tf
@@ -5,7 +5,7 @@ terraform {
 variable "secrets_dir" {}
 variable "charts_dir" {}
 variable "couchdb_prometheus_exporter_repository" {}
-variable "couchdb_prometheus_exporter_checksum" {}
+variable "couchdb_prometheus_exporter_tag" {}
 variable "couchdb_prometheus_exporter_p2sd_repository" {}
 variable "couchdb_prometheus_exporter_p2sd_tag" {}
 
@@ -25,7 +25,7 @@ data "template_file" "couchdb_prometheus_exporter_values" {
     couchdb_admin_username                      = "${var.secret_couchdb_admin_username}"
     couchdb_admin_password                      = "${var.secret_couchdb_admin_password}"
     couchdb_prometheus_exporter_repository      = "${var.couchdb_prometheus_exporter_repository}"
-    couchdb_prometheus_exporter_checksum        = "${var.couchdb_prometheus_exporter_checksum}"
+    couchdb_prometheus_exporter_tag        = "${var.couchdb_prometheus_exporter_tag}"
     couchdb_prometheus_exporter_p2sd_repository = "${var.couchdb_prometheus_exporter_p2sd_repository}"
     couchdb_prometheus_exporter_p2sd_tag        = "${var.couchdb_prometheus_exporter_p2sd_tag}"
     replica_count                               = "${var.replica_count}"
