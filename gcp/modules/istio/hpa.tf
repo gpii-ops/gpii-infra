e49bcd40aeb8cb5a03f0b9bc7963403490a755d6
Stepan Stipl <stepan@stipl.net>
2019-06-13 15:09:05 +0100

    GPII-3748: Add istio Terraform module

---
+++ gcp/modules/istio/hpa.tf
@@ -0,0 +1,119 @@
+resource "kubernetes_horizontal_pod_autoscaler" "istio-egressgateway" {
+  metadata = {
+    name      = "istio-egressgateway"
+    namespace = "istio-system"
+
+    labels = {
+      k8s-app = "istio"
+    }
+  }
+
+  spec {
+    max_replicas = 5
+    min_replicas = 2
+
+    scale_target_ref {
+      kind        = "Deployment"
+      name        = "istio-egressgateway"
+      api_version = "apps/v1beta1"
+    }
+
+    target_cpu_utilization_percentage = 80
+  }
+}
+
+resource "kubernetes_horizontal_pod_autoscaler" "istio-ingressgateway" {
+  metadata = {
+    name      = "istio-ingressgateway"
+    namespace = "istio-system"
+
+    labels = {
+      k8s-app = "istio"
+    }
+  }
+
+  spec {
+    max_replicas = 5
+    min_replicas = 3
+
+    scale_target_ref {
+      kind        = "Deployment"
+      name        = "istio-ingressgateway"
+      api_version = "apps/v1beta1"
+    }
+
+    target_cpu_utilization_percentage = 80
+  }
+}
+
+resource "kubernetes_horizontal_pod_autoscaler" "istio-pilot" {
+  metadata = {
+    name      = "istio-pilot"
+    namespace = "istio-system"
+
+    labels = {
+      k8s-app = "istio"
+    }
+  }
+
+  spec {
+    max_replicas = 5
+    min_replicas = 2
+
+    scale_target_ref {
+      kind        = "Deployment"
+      name        = "istio-pilot"
+      api_version = "apps/v1beta1"
+    }
+
+    target_cpu_utilization_percentage = 80
+  }
+}
+
+resource "kubernetes_horizontal_pod_autoscaler" "istio-policy" {
+  metadata = {
+    name      = "istio-policy"
+    namespace = "istio-system"
+
+    labels = {
+      k8s-app = "istio"
+    }
+  }
+
+  spec {
+    max_replicas = 5
+    min_replicas = 2
+
+    scale_target_ref {
+      kind        = "Deployment"
+      name        = "istio-policy"
+      api_version = "apps/v1beta1"
+    }
+
+    target_cpu_utilization_percentage = 80
+  }
+}
+
+resource "kubernetes_horizontal_pod_autoscaler" "istio-telemetry" {
+  metadata = {
+    name      = "istio-telemetry"
+    namespace = "istio-system"
+
+    labels = {
+      k8s-app = "istio"
+    }
+  }
+
+  spec {
+    max_replicas = 5
+    min_replicas = 2
+
+    scale_target_ref {
+      kind        = "Deployment"
+      name        = "istio-telemetry"
+      api_version = "apps/v1beta1"
+    }
+
+    target_cpu_utilization_percentage = 80
+  }
+}
