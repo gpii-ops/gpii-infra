865bd9f6a7812b99c5023f237f5b368d38851bb4
Alfredo Matas <amatas@gmail.com>
2019-08-01 19:21:34 +0200

    Add note about AWS provider

--- common/modules/gcp-dns/main.tf
+++ common/modules/gcp-dns/main.tf
@@ -25,6 +25,8 @@ provider "google" {
   region      = "${var.infra_region}"
 }
 
+# This provider is still needed for manage the AWS resources
+# it needs to be removed before GPII-2883 PR is merged
 provider "aws" {
   version = "~> 1.8"
   region  = "us-east-2"
