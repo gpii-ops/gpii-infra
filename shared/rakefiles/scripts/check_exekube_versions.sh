74cdbe4297224f3838349fee0a50cc8047237a22
Sergey Nazarov <natarajaya@gmail.com>
2019-08-19 15:09:09 -0700

    Use spaces

--- shared/rakefiles/scripts/check_exekube_versions.sh
+++ shared/rakefiles/scripts/check_exekube_versions.sh
@@ -9,20 +9,20 @@ images=$(find . \( -name "$EXCLUDE_DIRS" -prune \) -o -type f -exec grep -h -o -
 images_count=$(echo "$images" | wc -l | awk '{ print $1 }')
 
 if [ "$images" = "" ]; then
-	echo "Can't find any matches, skipping..."
+  echo "Can't find any matches, skipping..."
 elif [ "$images_count" -eq 1 ]; then
   echo "Check passed!"
   echo
   echo "Only $(echo "$images" | tr -d '\n') is being used."
 else
-	images_with_files=$(find . \( -name "$EXCLUDE_DIRS" -prune \) -o -type f -exec grep -n -o -E -H "$IMAGE_REGEX" {} \; | sort -u)
+  images_with_files=$(find . \( -name "$EXCLUDE_DIRS" -prune \) -o -type f -exec grep -n -o -E -H "$IMAGE_REGEX" {} \; | sort -u)
 
-	echo "Check failed!"
-	echo
-	echo "$images_count image versions:"
-	echo "$images"
-	echo
-	echo "Found in files:"
-	echo "$images_with_files"
-	exit 1
+  echo "Check failed!"
+  echo
+  echo "$images_count image versions:"
+  echo "$images"
+  echo
+  echo "Found in files:"
+  echo "$images_with_files"
+  exit 1
 fi
