15184dcda440065a04de9069c2eb1e8c22929aa2
Sergey Nazarov <natarajaya@gmail.com>
2019-08-19 14:36:44 -0700

    Rename var, improve condition

--- shared/rakefiles/scripts/check_exekube_versions.sh
+++ shared/rakefiles/scripts/check_exekube_versions.sh
@@ -3,17 +3,19 @@
 # This script verifies that only one version of exekube is being used across the project
 
 EXCLUDE_DIRS=".git"
-IMAGE_PATTERN="gpii/exekube:\d+\.\d+\.\d+-google_gpii\.\d+"
+IMAGE_REGEX="gpii/exekube:\d+\.\d+\.\d+-google_gpii\.\d+"
 
-images=$(find . \( -name "$EXCLUDE_DIRS" -prune \) -o -type f -exec grep -h -o -E "$IMAGE_PATTERN" {} \; | sort -u)
+images=$(find . \( -name "$EXCLUDE_DIRS" -prune \) -o -type f -exec grep -h -o -E "$IMAGE_REGEX" {} \; | sort -u)
 images_count=$(echo "$images" | wc -l | awk '{ print $1 }')
 
-if [ "$images_count" -eq 1 ]; then
+if [ "$images" == "" ]; then
+	echo "Can't find any matches, skipping..."
+elif [ "$images_count" -eq 1 ]; then
   echo "Check passed!"
   echo
   echo "Only $(echo "$images" | tr -d '\n') is being used."
 else
-	images_with_files=$(find . \( -name "$EXCLUDE_DIRS" -prune \) -o -type f -exec grep -n -o -E -H "$IMAGE_PATTERN" {} \; | sort -u)
+	images_with_files=$(find . \( -name "$EXCLUDE_DIRS" -prune \) -o -type f -exec grep -n -o -E -H "$IMAGE_REGEX" {} \; | sort -u)
 
 	echo "Check failed!"
 	echo
