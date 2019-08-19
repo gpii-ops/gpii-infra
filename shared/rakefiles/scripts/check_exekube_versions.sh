bffd5237a565725df67780ce47d5b68ac6c41629
Sergey Nazarov <natarajaya@gmail.com>
2019-08-19 14:17:36 -0700

    GNU grep can not exclude dirs

--- shared/rakefiles/scripts/check_exekube_versions.sh
+++ shared/rakefiles/scripts/check_exekube_versions.sh
@@ -2,23 +2,25 @@
 
 # This script verifies that only one version of exekube is being used across the project
 
-IMAGE_PATTERN="gpii/exekube:\d+\.\d+\.\d+-google_gpii\.\d+"
 EXCLUDE_DIRS=".git"
+IMAGE_PATTERN="gpii/exekube:\d+\.\d+\.\d+-google_gpii\.\d+"
 
-images=$(grep -r -o -E -h "$IMAGE_PATTERN" --exclude-dir "$EXCLUDE_DIRS" . | sort -u)
-images_count=$(echo "$images" | wc -l | bc)
+images=$(find . \( -name "$EXCLUDE_DIRS" -prune \) -o -type f -exec grep -h -o -E "$IMAGE_PATTERN" {} \; | sort -u)
+images_count=$(echo "$images" | wc -l | awk '{ print $1 }')
 
 if [ "$images_count" -eq 1 ]; then
   echo "Check passed!"
   echo
   echo "Only $(echo "$images" | tr -d '\n') is being used."
 else
+	images_with_files=$(find . \( -name "$EXCLUDE_DIRS" -prune \) -o -type f -exec grep -n -o -E -H "$IMAGE_PATTERN" {} \; | sort -u)
+
 	echo "Check failed!"
 	echo
 	echo "$images_count image versions:"
 	echo "$images"
 	echo
 	echo "Found in files:"
-	grep -r -o -E -n "$IMAGE_PATTERN" --exclude-dir "$EXCLUDE_DIRS" . | sort -u
+	echo "$images_with_files"
 	exit 1
 fi
