8d3da71df42605550172e9bf67951e3be16d897d
Alfredo Matas <amatas@gmail.com>
2019-09-26 22:24:51 +0200

    Use less tf resources because deps are broken

--- gcp/modules/gcp-stackdriver-monitoring/main.tf
+++ gcp/modules/gcp-stackdriver-monitoring/main.tf
@@ -43,47 +43,8 @@ resource "template_dir" "resources" {
   }
 }
 
-resource "null_resource" "destroy_stackdriver_lbm" {
-  triggers = {
-    nonce = "${var.nonce}"
-  }
-
-  provisioner "local-exec" {
-    command = <<EOF
-      export PROJECT_ID=${var.project_id}
-      export GOOGLE_CLOUD_KEYFILE=${var.serviceaccount_key}
-
-      RETRIES=5
-      RETRY_COUNT=1
-      while [ "$STACKDRIVER_DEADLINE_EXCEEDED" != "false" ]; do
-        STACKDRIVER_DEADLINE_EXCEEDED="false"
-        echo "[Try $RETRY_COUNT of $RETRIES] Destroying Stackdriver resources..."
-        bundle exec ruby -e '
-          require "/rakefiles/stackdriver.rb"
-          destroy_resources({"log_based_metrics"=>[]})
-        '
-        STACKDRIVER_EXIT_STATUS="$?"
-        if [ "$STACKDRIVER_EXIT_STATUS" == "120" ]; then
-          STACKDRIVER_DEADLINE_EXCEEDED="true"
-        elif [ "$STACKDRIVER_EXIT_STATUS" != "0" ]; then
-          exit $STACKDRIVER_EXIT_STATUS
-        fi
-        if [ "$RETRY_COUNT" == "$RETRIES" ] && [ "$STACKDRIVER_DEADLINE_EXCEEDED" == "true" ]; then
-          echo "Retry limit reached, giving up!"
-          exit 1
-        fi
-        if [ "$STACKDRIVER_DEADLINE_EXCEEDED" == "true" ]; then
-          sleep 10
-        fi
-        RETRY_COUNT=$(($RETRY_COUNT+1))
-      done
-    EOF
-  }
-}
-
-
 resource "null_resource" "destroy_stackdriver_monitoring" {
-  depends_on = ["template_dir.resources", "null_resource.destroy_stackdriver_lbm"]
+  depends_on = ["template_dir.resources"]
 
   triggers = {
     nonce = "${var.nonce}"
@@ -103,6 +64,31 @@ resource "null_resource" "destroy_stackdriver_monitoring" {
           require "/rakefiles/stackdriver.rb"
           destroy_resources({"alert_policies"=>[],"notification_channels"=>[]})
           destroy_uptime_checks(["${join("\",\"", google_monitoring_uptime_check_config.this.*.name)}"])
+        '
+        STACKDRIVER_EXIT_STATUS="$?"
+        if [ "$STACKDRIVER_EXIT_STATUS" == "120" ]; then
+          STACKDRIVER_DEADLINE_EXCEEDED="true"
+        elif [ "$STACKDRIVER_EXIT_STATUS" != "0" ]; then
+          exit $STACKDRIVER_EXIT_STATUS
+        fi
+        if [ "$RETRY_COUNT" == "$RETRIES" ] && [ "$STACKDRIVER_DEADLINE_EXCEEDED" == "true" ]; then
+          echo "Retry limit reached, giving up!"
+          exit 1
+        fi
+        if [ "$STACKDRIVER_DEADLINE_EXCEEDED" == "true" ]; then
+          sleep 10
+        fi
+        RETRY_COUNT=$(($RETRY_COUNT+1))
+      done
+
+      RETRIES=5
+      RETRY_COUNT=1
+      while [ "$STACKDRIVER_DEADLINE_EXCEEDED" != "false" ]; do
+        STACKDRIVER_DEADLINE_EXCEEDED="false"
+        echo "[Try $RETRY_COUNT of $RETRIES] Destroying Stackdriver resources..."
+        bundle exec ruby -e '
+          require "/rakefiles/stackdriver.rb"
+          destroy_resources({"log_based_metrics"=>[]})
         '
         STACKDRIVER_EXIT_STATUS="$?"
         if [ "$STACKDRIVER_EXIT_STATUS" == "120" ]; then
