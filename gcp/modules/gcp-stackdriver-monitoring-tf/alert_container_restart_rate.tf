00343ef3e3106653bf2782c4eac7cc0e1614acd2
Alfredo Matas <amatas@gmail.com>
2019-09-26 17:16:25 +0200

    Modify the alignment period and series of container restart

--- gcp/modules/gcp-stackdriver-monitoring-tf/alert_container_restart_rate.tf
+++ gcp/modules/gcp-stackdriver-monitoring-tf/alert_container_restart_rate.tf
@@ -1,25 +1,28 @@
 resource "google_monitoring_alert_policy" "container_restart_rate" {
-  display_name = "K8s containers does not restart more often than 1.5 times per minute"
+  display_name = "K8s containers does not restart more often than 2 times per minute in 5 min"
   combiner     = "OR"
 
   conditions {
     condition_threshold {
       filter          = "metric.type=\"kubernetes.io/container/restart_count\" resource.type=\"k8s_container\""
       comparison      = "COMPARISON_GT"
-      threshold_value = 1.5
-      duration        = "0s"
+      threshold_value = 2
+      duration        = "300s"
 
       aggregations {
         alignment_period   = "60s"
+        cross_series_reducer = "REDUCE_SUM"
         per_series_aligner = "ALIGN_DELTA"
-        group_by_fields    = []
+        group_by_fields    = [
+          "resource.label.container_name"
+        ]
       }
 
       denominator_filter       = ""
       denominator_aggregations = []
     }
 
-    display_name = "K8s container restarting more often than 1.5 times per minute"
+    display_name = "K8s container is restarting more often than 2 times per minute during 5 min"
   }
 
   notification_channels = ["${google_monitoring_notification_channel.email.name}", "${google_monitoring_notification_channel.alerts_slack.*.name}"]
