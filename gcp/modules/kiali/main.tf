4f0cf16fc25a23c172dd3c23d96ecd86a0e88ecd
Stepan Stipl <stepan@stipl.net>
2019-10-10 12:58:03 +0100

    GPII-4046: Use image via version-updater for Kiali chart

--- gcp/modules/kiali/main.tf
+++ gcp/modules/kiali/main.tf
@@ -4,6 +4,8 @@ terraform {
 
 variable "secrets_dir" {}
 variable "charts_dir" {}
+variable "kiali_repository" {}
+variable "kiali_tag" {}
 
 module "prometheus" {
   source           = "/exekube-modules/helm-release"
@@ -17,14 +19,24 @@ module "prometheus" {
   chart_name = "${var.charts_dir}/istio-prometheus"
 }
 
+data "template_file" "release_values" {
+  template = "${file("${path.module}/templates/values.yaml.tpl")}"
+
+  vars = {
+    kiali_repository = "${var.kiali_repository}"
+    kiali_tag        = "${var.kiali_tag}"
+  }
+}
+
 module "kiali" {
   source           = "/exekube-modules/helm-release"
   tiller_namespace = "kube-system"
   client_auth      = "${var.secrets_dir}/kube-system/helm-tls"
 
-  release_name      = "kiali"
-  release_namespace = "istio-system"
-  release_values    = ""
+  release_name            = "kiali"
+  release_namespace       = "istio-system"
+  release_values          = ""
+  release_values_rendered = "${data.template_file.release_values.rendered}"
 
   chart_name = "${var.charts_dir}/istio-kiali"
 }
