9042e1aecee40605cbe3d69da17b550ce6777fb8
Alfredo Matas <amatas@gmail.com>
2019-10-21 18:15:32 +0200

    Unroll refactor due to notification channels resources constraint

--- common/modules/gcp-stackdriver/notification_alerts_slack.tf
+++ common/modules/gcp-stackdriver/notification_alerts_slack.tf
@@ -1,10 +1,21 @@
-resource "google_monitoring_notification_channel" "alerts_slack" {
-  count        = "${length(var.common_environments)}"
+resource "google_monitoring_notification_channel" "alerts_slack_prd" {
   type         = "slack"
-  display_name = "Alerts ${element(local.common_environments, count.index)} Slack"
+  display_name = "Alerts prd Slack"
 
   labels = {
-    channel_name = "#alerts-${element(local.common_environments, count.index)}"
+    channel_name = "#alerts-prd"
+  }
+
+  user_labels = {}
+  enabled     = "true"
+}
+
+resource "google_monitoring_notification_channel" "alerts_slack_stg" {
+  type         = "slack"
+  display_name = "Alerts stg Slack"
+
+  labels = {
+    channel_name = "#alerts-stg"
   }
 
   user_labels = {}
