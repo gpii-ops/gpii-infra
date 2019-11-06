d8c7e4ed9ba9314de28361ccf2658c98509c6197
Alfredo Matas <amatas@gmail.com>
2019-11-06 14:24:23 +0100

    GPII-4220 Change display name of alert Slack channel

--- gcp/modules/gcp-stackdriver-monitoring/notification_alerts_slack.tf
+++ gcp/modules/gcp-stackdriver-monitoring/notification_alerts_slack.tf
@@ -2,7 +2,7 @@ resource "google_monitoring_notification_channel" "alerts_slack" {
   depends_on   = ["null_resource.destroy_old_stackdriver_resources"]
   count        = "${(var.env == "prd" || var.env == "stg") ? 1 : 0}"
   type         = "slack"
-  display_name = "Alerts #${var.env} Slack"
+  display_name = "#alerts-${var.env}"
 
   labels = {
     channel_name = "#alerts-${var.env}"
