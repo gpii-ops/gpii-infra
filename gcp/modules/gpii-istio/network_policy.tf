bed134f56e881dd64df002a94aeb0e74857633af
Stepan Stipl <stepan@stipl.net>
2019-11-14 12:55:56 +0000

    GPII-4102: Block egress traffic from gpii namespace

--- gcp/modules/gpii-istio/network_policy.tf
+++ gcp/modules/gpii-istio/network_policy.tf
@@ -5,9 +5,57 @@ resource "kubernetes_network_policy" "deny-default" {
   }
 
   spec {
-    pod_selector = {}
+    pod_selector {}
 
-    policy_types = ["Ingress"]
+    policy_types = ["Ingress", "Egress"]
+
+    egress {
+      # Allow egress traffic to Istio control plane
+      to {
+        namespace_selector {
+          match_labels = {
+            k8s-app = "istio"
+          }
+        }
+      }
+
+      # Allow egress traffic within namespace
+      to {
+        namespace_selector {
+          match_labels = {
+            name = "gpii"
+          }
+        }
+      }
+
+      # Allow egress traffic to dns
+      to {
+        namespace_selector {
+          match_labels = {
+            name = "kube-system"
+          }
+        }
+        pod_selector {
+          match_labels = {
+            k8s-app = "kube-dns"
+          }
+        }
+      }
+    }
+
+    egress {
+      # Allow egress traffic Compute Metadata
+      to {
+        ip_block {
+          cidr = "10.16.0.0/20"
+        }
+      }
+      ports {
+        port     = "8181"
+        protocol = "TCP"
+      }
+    }
+
   }
 }
 
