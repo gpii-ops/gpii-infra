bf01fe8b94ac1f78facfc107fadad2700f176bed
Sergey Nazarov <natarajaya@gmail.com>
2019-12-20 09:54:53 -0800

    Update resource names

--- gcp/modules/gcp-stackdriver-export/gpii_key.tf
+++ gcp/modules/gcp-stackdriver-export/gpii_key.tf
@@ -3,20 +3,20 @@ provider "google" {
   credentials = "${var.serviceaccount_key}"
 }
 
-resource "google_bigquery_dataset" "this" {
+resource "google_bigquery_dataset" "gpii_key" {
   dataset_id = "exported_logs_with_gpii_key"
   location   = "US"
 }
 
-resource "google_logging_project_sink" "this" {
+resource "google_logging_project_sink" "gpii_key" {
   name        = "gpii-key"
-  destination = "bigquery.googleapis.com/projects/${var.project_id}/datasets/${google_bigquery_dataset.this.dataset_id}"
+  destination = "bigquery.googleapis.com/projects/${var.project_id}/datasets/${google_bigquery_dataset.gpii_key.dataset_id}"
   filter      = "textPayload:\"gpiiKey: '\" AND resource.type=\"k8s_container\" AND (resource.labels.container_name=\"preferences\" OR resource.labels.container_name=\"flowmanager\")"
 
   unique_writer_identity = true
 }
 
-resource "google_project_iam_member" "this" {
-  member = "${google_logging_project_sink.this.writer_identity}"
+resource "google_project_iam_member" "gpii_key" {
+  member = "${google_logging_project_sink.gpii_key.writer_identity}"
   role   = "roles/bigquery.dataEditor"
 }
